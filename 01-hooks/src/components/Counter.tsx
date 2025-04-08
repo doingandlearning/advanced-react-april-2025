313031a153d5efcc44659ab253162910c68fd8c9
Kevin Cunningham <github@kevincunningham.co.uk>
2025-04-08 11:51:47 +0100

    adds type

--- 01-hooks/src/components/Counter.tsx
+++ 01-hooks/src/components/Counter.tsx
@@ -2,7 +2,9 @@ import { useReducer } from 'react';
 
 const initialState = { count: 0 };
 
-function reducer(state: typeof initialState, action: { type: string }) {
+type ReducerActions = "increment" | "decrement"
+
+function reducer(state: typeof initialState, action: { type: ReducerActions }) {
   switch (action.type) {
     case 'increment':
       return { count: state.count + 1 };
